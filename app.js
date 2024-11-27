766d98a9f301d3746c33d140f34aa9df7af337ac
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-11-27 21:27:38 +0100

    Added PUT route

--- app.js
+++ app.js
@@ -35,6 +35,33 @@ app.post("/books", async (req, res) => {
   }
 });
 
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, year } = req.body;
+
+    const updatedRowsCount = await Book.update(
+      {
+        title,
+        author,
+        year,
+      },
+      {
+        where: { id },
+      }
+    );
+
+    if (updatedRowsCount === 0) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json({ message: "Book updated successfully" });
+  } catch (error) {
+    console.error("Error updating  books:", error);
+    res.status(500).json({ message: "Failed to update books" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
