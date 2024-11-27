a76dec95c3e805af537e7d004bf3ccb62f49116a
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-11-27 20:23:11 +0100

    Added migration book

---
+++ migrations/20241127191247-create-books-table.cjs
@@ -0,0 +1,37 @@
+"use strict";
+
+const { DataTypes } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Books", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Books");
+  },
+};
