dbb255426f3ac9a759a0c622b6a1c55ce518816a
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-11-27 20:35:38 +0100

    Added GET route

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
+import Book from "./models/book.js";
+import sequelize from "./config/db.js";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.use(express.json());
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.json(books);
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    res.status(500).json({ message: "Failed to fetch books" });
+  }
 });
 
 app.listen(port, () => {
